86fa1106b56a66fa99f0b0cf64b98f287d264fe2
aswanthsasi333-dev <aswanthsasi333@gmail.com>
2026-02-25 23:39:34 -0800

    project created

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # JWT Login
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    # Refresh Token
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
